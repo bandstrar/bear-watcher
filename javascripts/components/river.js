17226bbfad990c129d4bda9bd0a130e8499e9e2c
bandstrar <rob.bandstra@gmail.com>
2020-08-22 11:01:03 -0500

    removed commented out code

--- javascripts/components/river.js
+++ javascripts/components/river.js
@@ -8,19 +8,6 @@ const buildABear = () => {
     })
   }
 
-// const bearCards = (bear) => {
-//   for (let i = 0; i < bear.length; i++) {
-//     $('#bearCards').append(
-//           `<div class="card" style="width: 18rem;">
-//           <img class="card-img-top" src="${bear[i].image}" alt="A Bear">
-//           <div class="card-body">
-//             <p class="card-text">${bear[i].name}</p>
-//           </div>
-//         </div>`
-//     )
-//   }
-// }
-
 const bearCards = () => {
 bearArray.forEach(bear => {
   $('#bearCards').append(
