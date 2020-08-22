11a5c4c8449c1d035b0a3e155eaf8d996748f034
bandstrar <rob.bandstra@gmail.com>
2020-08-22 14:33:20 -0500

    added clickable attempt button and non-clickable success button

--- javascripts/components/river.js
+++ javascripts/components/river.js
@@ -9,16 +9,30 @@ const buildABear = () => {
   }
 
 const bearCards = () => {
-bearArray.forEach(bear => {
+bearArray.forEach((bear, i) => {
   $('#bearCards').append(
     `<div class="card" style="width: 18rem;">
       <img class="card-img-top" src="${bear.image}" alt="A Bear">
         <div class="card-body">
         <p class="card-text">${bear.name}</p>
+        <div id="attempt-date--${i}"></div>
+        <button class="attempt--${i}"><i class="fas fa-fish"></i></button>
+        <button class="success--${i}"><i class="fas fa-check"></i></button>
       </div>
     </div>`
   )
+  attemptCheck(i);
 })
 }
+
+const attemptCheck = (i) => {
+  $(`.attempt--${i}`).on('click', () => {
+    $(`#attempt-date--${i}`).append(
+      Date()
+    )
+  })
+}
+
+
 
 export {buildABear}
