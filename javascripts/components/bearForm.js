baa141573b76b8423f479c23df0d04e7fbc49254
bandstrar <rob.bandstra@gmail.com>
2020-08-22 10:42:16 -0500

    submit button now works properly, bears cards properly printing

--- javascripts/components/bearForm.js
+++ javascripts/components/bearForm.js
@@ -4,9 +4,9 @@ let bearArray = []
 
 const buildABear = () => {
     $('#bearButton').on('click', () => {
-      bearArray.push({name: bearName.value, image: bearImage.value})
+      bearArray.push({name: bearName.value, image: bearImage.value});
+      $('#bearCards').empty();
       bearCards(bearArray);
-      console.log(bearArray);
     })
   }
 
@@ -14,14 +14,14 @@ const bearForm = () => {
     $('#bearForm').html(
         `<form>
     <div class="form-group">
-      <label for="bearName">Bear Name</label>
+      <label for="bearName" class="form--text">Bear Name</label>
       <input type="text" class="form-control" id="bearName" aria-describedby="emailHelp" placeholder="Enter Bear Name">
     </div>
     <div class="form-group">
-      <label for="bearImage">Bear Image</label>
+      <label for="bearImage" class="form--text">Bear Image</label>
       <input type="text" class="form-control" id="bearImage" placeholder="Enter Bear Image URL">
     </div>
-    <input type="button" class="btn btn-primary" value="Submit" id="bearButton"></input>
+    <input type="button" class="btn btn-dark" value="Submit" id="bearButton"></input>
   </form>`
     )
 }
--- javascripts/components/river.js
+++ javascripts/components/river.js
@@ -1,14 +1,14 @@
 const bearCards = (bear) => {
-    for (let i = 0; i < bear.length; i++) {
-        $('#bearCards').html(
-            `<div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="${bear[i].image}" alt="Card image cap">
-            <div class="card-body">
-              <p class="card-text">${bear[i].name}</p>
-            </div>
-          </div>`
-        )
-    }
+  for (let i = 0; i < bear.length; i++) {
+    $('#bearCards').append(
+          `<div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="${bear[i].image}" alt="A Bear">
+          <div class="card-body">
+            <p class="card-text">${bear[i].name}</p>
+          </div>
+        </div>`
+    )
+  }
 }
 
 export {bearCards}
