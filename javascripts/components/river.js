29877d90e3bb1fa01a94c0d89b4d0f3f13d45259
bandstrar <rob.bandstra@gmail.com>
2020-08-23 21:45:18 -0500

    added buttons that track when bear attempts and succeeds at catching fish, and tally of number of fish caught

--- javascripts/components/river.js
+++ javascripts/components/river.js
@@ -3,13 +3,14 @@ let bearArray = []
 const buildABear = () => {
     $('#bearButton').on('click', () => {
       bearArray.push({name: bearName.value, image: bearImage.value});
-      $('#bearCards').empty();
       bearCards();
     })
   }
 
 const bearCards = () => {
+  const lastBear = (bearArray.length - 1)
 bearArray.forEach((bear, i) => {
+  if (i === lastBear) {
   $('#bearCards').append(
     `<div class="card" style="width: 18rem;">
       <img class="card-img-top" src="${bear.image}" alt="A Bear">
@@ -18,17 +19,34 @@ bearArray.forEach((bear, i) => {
         <div id="attempt-date--${i}"></div>
         <button class="attempt--${i}"><i class="fas fa-fish"></i></button>
         <button class="success--${i}"><i class="fas fa-check"></i></button>
+        <p class="running-tally">Caught fish:<div id="running-tally--${i}"></div></p>
       </div>
     </div>`
   )
   attemptCheck(i);
+  successCheck(i);
+  }
 })
 }
 
 const attemptCheck = (i) => {
   $(`.attempt--${i}`).on('click', () => {
     $(`#attempt-date--${i}`).append(
-      Date()
+      `<p>Attempted to catch fish on ${Date()}</p>`
+    )
+  })
+}
+
+const successCheck = (i) => {
+  let runningTotal = 0
+
+  $(`.success--${i}`).on('click', () => {
+    $(`#attempt-date--${i}`).append(
+      `<p>Successfully caught fish on ${Date()}</p>`
+    )
+    runningTotal++
+    $(`#running-tally--${i}`).html(
+      `${runningTotal}`
     )
   })
 }
